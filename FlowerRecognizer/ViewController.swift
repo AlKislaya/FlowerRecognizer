676d7bf63a8f34abd8999abeac2aed15fdaa3187
alkislaya <41301020+AlKislaya@users.noreply.github.com>
2026-01-15 12:19:58 +0300

    Initial Commit

---
+++ FlowerRecognizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlowerRecognizer
+//
+//  Created by Alexandra on 15.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
