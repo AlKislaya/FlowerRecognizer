06d3d2d1c9d45de69a4d2385efe5abbb4bc8b672
alkislaya <41301020+AlKislaya@users.noreply.github.com>
2026-01-16 17:38:27 +0300

    Integrate image analyzation into view controller logic

--- FlowerRecognizer/Main View/ViewController.swift
+++ FlowerRecognizer/Main View/ViewController.swift
@@ -8,11 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var predictionLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    let flowersAnalyzer = FlowersAnalyzer()
+    
     func onUserPickedImage(_ image: UIImage) {
+        updateImageView(image)
+        updatePrediction("Making predictions for the photo...")
+        
+        do {
+            try flowersAnalyzer.analyze(for: image, completionHandler: onImageAnalyzed)
+        } catch {
+            updatePrediction("Smth went wrong")
+        }
+    }
+    
+    func onImageAnalyzed(category: String?, error: Error?) {
+        if let error = error {
+            updatePrediction(error.localizedDescription)
+            return
+        }
+        
+        updatePrediction(category ?? "Smth went wrong")
+    }
+    
+    func updateImageView(_ image: UIImage) {
         DispatchQueue.main.async {
             self.imageView.image = image
+        }
+    }
+    
+    func updatePrediction(_ text: String) {
+        DispatchQueue.main.async {
+            self.predictionLabel.text = text
         }
     }
 
